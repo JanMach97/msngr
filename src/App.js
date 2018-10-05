dbfa420bc9c89975543d11cff336a552555feae5
JanMach97 <janmachacek97@gmail.com>
2018-10-05 22:16:25 +0100

    add components layoout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Profile from './components/Profile';
+import Users from './components/Users';
+import Chatbox from './components/Chatbox';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Profile />
+        <Users />
+        <Chatbox />
+        <Navbar />
       </div>
     );
   }
