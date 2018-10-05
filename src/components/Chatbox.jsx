96880adaf14a9ff5805d08503fab27044d354316
JanMach97 <janmachacek97@gmail.com>
2018-10-05 22:12:29 +0100

    add chatbox component

---
+++ src/components/Chatbox.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Chatbox extends Component {
+  render() {
+    return (
+      <div>
+        <h1>chatbox</h1>
+      </div>
+    );
+  }
+}
+
+export default Chatbox;
